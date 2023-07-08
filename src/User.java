4a2c822be2c8a0bbba7fa6073d308d232c5be5e9
MarharytaKohut <mrhrtkohut@gmail.com>
2023-07-08 20:34:35 +0300

    commit

---
+++ src/User.java
@@ -0,0 +1,19 @@
+public class User {
+    private String login;
+    Account account;
+    public User(String login, String password){
+        this.login = login;
+        this.account = new Account(password);
+    }
+    class Account{
+        private String password;
+
+        public Account(String password) {
+            this.password = password;
+        }
+
+        void displayAccount(){
+            System.out.println("Account Login successful! Login: " + User.this.login + ", Password: " + password + ".");
+        }
+    }
+}
